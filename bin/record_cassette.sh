efa723205fbdd4d89a275d9fed55b1e88c719586
grantj-re3 <grantj-re3@github.fake>
2018-06-02 15:28:13 +0930

    Modify record_cassette.sh to specify duration via H:M:S from command line or interactive prompt. Do not permit a default duration or a --test option.

--- bin/record_cassette.sh
+++ bin/record_cassette.sh
@@ -27,12 +27,10 @@ app=`basename $0`
 # For Ezcap Super USB Cassette Capture:
 #  Fedora 17: alsa_input.usb-Solid_State_System_Co._Ltd._USB_PnP_Audio_Device-00-Device_1.analog-stereo
 #  Fedora 20: alsa_input.usb-Solid_State_System_Co._Ltd._USB_PnP_Audio_Device-00-Device.analog-stereo
+#  Fedora 25: alsa_input.usb-Solid_State_System_Co._Ltd._USB_PnP_Audio_Device-00.analog-stereo
 PA_SOURCE_RE="alsa_input.usb-Solid_State_System_Co._Ltd._USB_PnP_Audio_Device-"
+PA_SOURCE_LABEL="USB Cassette"
 PA_VOLUME=65536		# 0=mute; 65536=100% (ie. max without clipping)
-
-DEFAULT_NOM_MINUTES=45	# Nominal duration of audio cassette side to be recorded
-SECONDS_EXTRA=90	# Extra seconds to add to minutes to be recorded
-SECONDS_TEST=15		# Total seconds to record in test-mode
 
 DEFAULT_DIR=$HOME/rec_cassette
 FNAME_PREFIX="recording."
@@ -50,41 +48,40 @@ usage_exit() {
   cat <<-EO_USAGE_MSG |sed 's/^	//' >&2
 	$msg
 
-	Usage:  $app  [-m 15|30|45|60] [-o OUT_MP3_FILENAME | -p]
-	Usage:  $app  --test|-t
-	Usage:  $app  --help|-h
+	Usage1:  $app  -d H:M:S [-o OUT_MP3_FILENAME]
+	Usage2:  $app  -p
+	Usage3:  $app  --help|-h
 
-	  -m specifies the nominal duration in [m]inutes of one side of the
-	     cassette to be recorded.
+	  -d specifies the [d]uration in H:M:S (hours:minutes:seconds) format
+	     where H, M and S can be any non-negative integer (eg. 0:91:30).
 	  -o specifies the [o]utput MP3 filename;
 	     default filename is "$FNAME4MSG";
 	     default directory is "$DEFAULT_DIR";
 	     you can write to your current directory using "-o ./OUT_MP3_FILENAME"
-	  -p causes the program to [p]rompt the user for the output MP3 filename.
-	  --test|-t causes the program to perform a [t]est recording for $SECONDS_TEST seconds.
+	  -p causes the program to [p]rompt the user for the duration and output
+	     MP3 filename.
 	  --help|-h shows this [h]elp message.
 
 	The names of the pulse-audio input devices connected now are:
 	EO_USAGE_MSG
-  pacmd list-sources |egrep "^[[:space:]]*name: "
+  pacmd list-sources |grep -P "^\s*name: "
   exit 1
 }
 
 ##############################################################################
 # get_cli_option(cli_args) -- Return the command line options in vars:
-#   copt_nom_minutes, copt_path, copt_prompt4path, copt_test
+#   copt_hms, copt_path, copt_prompt4params
 ##############################################################################
 get_cli_option() {
-  copt_nom_minutes="$DEFAULT_NOM_MINUTES"	# Assume the default nominal-minutes
+  copt_hms=""
   copt_path="$DEFAULT_PATH"		# Assume the default file path
-  copt_prompt4path=0			# Assume we will not prompt for path
-  copt_test=0				# Assume we will not use test-mode
+  copt_prompt4params=0			# Assume we will not prompt for path
 
   while [ $# -gt 0 ] ; do
     case "$1" in
-      -m )
+      -d )
         shift
-        copt_nom_minutes="$1"
+        copt_hms="$1"
         shift
         ;;
 
@@ -95,12 +92,7 @@ get_cli_option() {
         ;;
 
       -p )
-        copt_prompt4path=1
-        shift
-        ;;
-
-      --test | -t )
-        copt_test=1
+        copt_prompt4params=1
         shift
         ;;
 
@@ -118,42 +110,38 @@ get_cli_option() {
 # Setup program variables
 ##############################################################################
 setup_vars() {
-  copt_nom_minutes="$1"
-  copt_path="$2"
-  copt_prompt4path="$3"
-  copt_test="$4"
+  hms="$1"
+  path="$2"
+  copt_prompt4params="$3"
 
-  path="$copt_path"
+  [ $copt_prompt4params = 1 ] && prompt4params "$path"
   if ! echo "$path" |egrep -q "^/|^\."; then
-    path="$DEFAULT_DIR/$copt_path"	# Use default dir
-  fi
-
-  # Test mode
-  if [ $copt_test = 1 ]; then
-    seconds=$SECONDS_TEST
-    return
+    path="$DEFAULT_DIR/$path"	# Use default dir
   fi
 
   # Calculate number of seconds to record
-  if ! echo "$copt_nom_minutes" |egrep -q "^(15|30|45|60)$"; then
-    usage_exit "Nominal minutes must be one of 15,30,45,60."
+  if ! echo "$hms" |grep -q -P "^\d+:\d+:\d+$"; then
+    usage_exit "Duration must be in the format H:M:S"
   fi
-  seconds=`expr \( $copt_nom_minutes \* 60 \) + $SECONDS_EXTRA`
+  seconds=`echo "$hms" |awk -F: '{print $1*3600 + $2*60 + $3}'`
+}
 
-  # Prompt for file path
-  if [ $copt_prompt4path = 1 ]; then
-    echo     "[The default MP3 output filename is: $copt_path]"
-    echo -ne "Enter MP3 output filename [Press Enter for default]: "
-    read path			# Filename or absolute path or relative path
+##############################################################################
+# Prompt for parameters: H:M:S & path
+# Return: hms & path
+##############################################################################
+prompt4params() {
+  copt_path="$1"
 
-    if [ "$path" = "" ]; then
-      path="$copt_path"		# Use default or command line filename
+  echo -ne "Enter duration in the format 'H:M:S': "
+  read hms
 
-    elif ! echo "$path" |egrep -q "^/|^\."; then
-      path="$DEFAULT_DIR/$path"	# Use default dir
+  echo -e "\n[The default MP3 output filename is: $copt_path]"
+  echo -ne  "Enter MP3 output filename [Press Enter for default]: "
+  read path			# Filename or absolute path or relative path
 
-    fi
-  fi
+  [ "$path" = "" ] && path="$copt_path"		# Use default or command line filename
+  echo "--------------------"
 }
 
 ##############################################################################
@@ -169,7 +157,7 @@ make_dir() {
 ##############################################################################
 find_source_audio_device() {
   pa_source=`pacmd list-sources |
-    egrep "^[[:space:]]*name: " |
+    grep -P "^\s*name: " |
     egrep "$PA_SOURCE_RE" |
     tail -1 |
     sed 's/^[ 	]*name: *//; s/^<//; s/>$//'`
@@ -185,23 +173,26 @@ find_source_audio_device() {
 # main()
 ##############################################################################
 get_cli_option $@
-setup_vars "$copt_nom_minutes" "$copt_path" "$copt_prompt4path" "$copt_test"
+setup_vars "$copt_hms" "$copt_path" "$copt_prompt4params"
 find_source_audio_device
 
-echo -e "\nSetting USB cassette as default audio device..."
+echo -e "\n$PA_SOURCE_LABEL device is:"
+echo "  $pa_source"
+
+echo -e "\nSetting $PA_SOURCE_LABEL as default audio device..."
+
 pacmd set-default-source "$pa_source"
-echo -e "\n\nSetting USB cassette volume..."
+echo "Setting $PA_SOURCE_LABEL volume..."
 pacmd set-source-volume "$pa_source" $PA_VOLUME
 
 echo -e "\n\nWriting to file:  $path"
-if [ $copt_test = 1 ]; then
-  echo -e "Recording for:    $seconds seconds (for testing)\n\n"
-else
-  echo -e "Recording for:    $copt_nom_minutes minutes (plus $SECONDS_EXTRA seconds extra)\n\n"
-fi
+echo -e     "Recording for:    $seconds seconds (ie. Hr:Min:Sec = $hms)\n\n"
 
 make_dir "$DEFAULT_DIR"
-arecord -f cd -d $seconds -t raw |lame -r - "$path"
+cmd="arecord -f cd -d $seconds -t raw |lame -r - \"$path\""
+echo "Command: $cmd"
+echo
+eval $cmd
 
 echo -e "\nFINISHED!  Press any key to close program."
 read ans
